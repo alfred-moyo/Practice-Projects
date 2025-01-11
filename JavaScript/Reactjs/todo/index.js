984e872ae976b4b7f393e209d07efc253ad03a09
Alfred Moyo <147029100+alfred-moyo@users.noreply.github.com>
2025-01-11 21:54:50 +0400

    Update index.js

--- JavaScript/Reactjs/todo/index.js
+++ JavaScript/Reactjs/todo/index.js
@@ -31,3 +31,43 @@ class Todo extends React.Component {
         );
     }
 
+
+    handleChange(e) {
+        this.setState({ text: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if(!this.state.text.length) {
+            return;
+        }
+        const newItem = {
+            id: Date.now(),
+            text: this.state.text,
+        }
+        this.setState(prev => ({
+            items: prev.items.concat(newItem),
+            text: ''
+        }));
+    }
+}
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.items.map(item => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ul>
+        )
+    }
+}
+
+
+// ========================================
+
+ReactDOM.render(
+    <Todo />,
+    document.getElementById('root')
+);
