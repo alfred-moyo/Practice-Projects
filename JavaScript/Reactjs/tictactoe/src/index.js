355ceba25130ac41828d157457eb54d2a012cfe1
Alfred Moyo <147029100+alfred-moyo@users.noreply.github.com>
2025-02-03 21:00:34 +0400

    Update index.js

--- JavaScript/Reactjs/tictactoe/src/index.js
+++ JavaScript/Reactjs/tictactoe/src/index.js
@@ -74,3 +74,27 @@ class Board extends React.Component {
         } else {
             status = `Next player: ${this.state.xIsNext? 'X' : 'O'}`;
         }
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <button onClick={() => this.reset()}>Reset</button>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
